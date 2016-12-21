4e38fb5cc9caae8278cc43e7e6c7bc4ac2720895
David Sun <ffstudios1@gmail.com>
2016-12-21 11:42:37 -0800

    finiz

--- run.js
+++ run.js
@@ -17,9 +17,9 @@ const webpack = require('webpack');
 
 // TODO: Update configuration settings
 const config = {
-  title: 'React Static Boilerplate',        // Your website title
-  url: 'https://rsb.kriasoft.com',          // Your website URL
-  project: 'react-static-boilerplate',      // Firebase project. See README.md -> How to Deploy
+  title: 'Pigeon',        // Your website title
+  url: 'https://pigeon-f4c04.firebaseapp.com/',          // Your website URL
+  project: 'pigeon-f4c04',      // Firebase project. See README.md -> How to Deploy
   trackingID: 'UA-XXXXX-Y',                 // Google Analytics Site's ID
 };
 
@@ -96,43 +96,14 @@ tasks.set('build', () => {
 // Build and publish the website
 // -----------------------------------------------------------------------------
 tasks.set('publish', () => {
-  const remote = {
-    url: 'https://github.com/dave4506/pigeon.git', // TODO: Update deployment URL
-    branch: 'gh-pages',
-  };
-  global.DEBUG = process.argv.includes('--debug') || false;
-  const spawn = require('child_process').spawn;
-  const opts = { cwd: path.resolve(__dirname, './public'), stdio: ['ignore', 'inherit', 'inherit'] };
-  const git = (...args) => new Promise((resolve, reject) => {
-    spawn('git', args, opts).on('close', code => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(new Error(`git ${args.join(' ')} => ${code} (error)`));
-      }
-    });
-  });
-
-  return Promise.resolve()
-    .then(() => run('clean'))
-    .then(() => git('init', '--quiet'))
-    .then(() => git('config', '--get', 'remote.origin.url')
-      .then(() => git('remote', 'set-url', 'origin', remote.url))
-      .catch(() => git('remote', 'add', 'origin', remote.url))
-    )
-    .then(() => git('ls-remote', '--exit-code', remote.url, 'master')
-      .then(() => Promise.resolve()
-        .then(() => git('fetch', 'origin'))
-        .then(() => git('reset', `origin/${remote.branch}`, '--hard'))
-        .then(() => git('clean', '--force'))
-      )
-      .catch(() => Promise.resolve())
-    )
-    .then(() => run('build'))
-    .then(() => git('add', '.', '--all'))
-    .then(() => git('commit', '--message', new Date().toUTCString())
-      .catch(() => Promise.resolve()))
-    .then(() => git('push', 'origin', `HEAD:${remote.branch}`, '--force', '--set-upstream'));
+  const firebase = require('firebase-tools');
+  return run('build')
+    .then(() => firebase.login({ nonInteractive: false }))
+    .then(() => firebase.deploy({
+      project: config.project,
+      cwd: __dirname,
+    }))
+    .then(() => { setTimeout(() => process.exit()); });
 });
 
 //
